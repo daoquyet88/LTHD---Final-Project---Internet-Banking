33a6cd3c48f9f53fc91dc0ca810aded68cd4dcd1
daoquyet88 <49382785+daoquyet88@users.noreply.github.com>
2020-06-05 23:06:33 +0700

    kiêm tra nhap lieu
    kiem tra thong tin nhap vao

--- bank/nhanvien.php
+++ bank/nhanvien.php
@@ -7,3 +7,33 @@
         echo "<script type='text/javascript'>alert('$message');</script>";
     }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/admin_login_style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+
+<body>
+    <form action="admin_login_action.php" method="post">
+        <div class="flex-container-1">
+            <div class="flex-item">
+                <h2>Đăng nhập quản trị viên</h2>
+            </div>
+
+            <label><b>Tài khoản</b></label>
+            <div class="flex-item">
+                <input type="text" name="admin_uname" required>
+            </div>
+
+            <label><b>Mật khẩu</b></label>
+            <div class="flex-item">
+                <input type="password" name="admin_psw" required>
+            </div>
+        </div>
+
+       
+    </form>
+</body>
+</html>
