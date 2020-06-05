0eec0eebf2c4f9ff7c8ce16dd9034da3f24416bd
daoquyet88 <49382785+daoquyet88@users.noreply.github.com>
2020-06-05 23:10:27 +0700

    Hoàn thiện chức năng sủa
    làm xong chức năng sửa

--- bank/nhanvien.php
+++ bank/nhanvien.php
@@ -51,6 +51,21 @@
                 <button type="button" class="cancelbtn" onclick="location.href='./home.php'">Quay về trang chủ</button>
             </div>
         </div>
+        <?php
+        $uname = mysqli_real_escape_string($conn, $_POST["admin_uname"]);
+    $pwd = mysqli_real_escape_string($conn, $_POST["admin_psw"]);
+
+    $sql0 =  "SELECT * FROM admin WHERE uname='".$uname."' AND pwd='".$pwd."'";
+    $result = $conn->query($sql0);
+
+    if (($result->num_rows) > 0) {
+        $_SESSION['isAdminValid'] = true;
+        $_SESSION['LAST_ACTIVITY'] = time();
+        header("location:admin_home.php");
+    } else {
+        session_destroy();
+        die(header("location:admin_login.php?loginFailed=true"));
+    }?>
     </form>
 </body>
 </html>
