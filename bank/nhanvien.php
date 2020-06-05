140ad25a0936d673370963937e4713abbf766bd8
daoquyet88 <49382785+daoquyet88@users.noreply.github.com>
2020-06-05 23:13:01 +0700

    cập nhập lại hiếm thị
    cập nhập code

--- bank/nhanvien.php
+++ bank/nhanvien.php
@@ -80,6 +80,22 @@
                 &emsp;<?php echo $type; ?>: <?php echo number_format($transaction); ?> VND<br>
                 &emsp;Thời gian: <?php echo $sanitized_time; ?><br>
                 &emsp;<?php echo $row1["remarks"]; ?>.
+            </p><?php
+            if (($result->num_rows) > 0) {
+        $_SESSION['isAdminValid'] = true;
+        $_SESSION['LAST_ACTIVITY'] = time();
+        header("location:admin_home.php");
+    } else {
+        session_destroy();
+        die(header("location:admin_login.php?loginFailed=true"));
+    }?>
+            <p id="customer">
+                &#9656 Số dư (VND): <?php echo number_format($row1["balance"]); ?><br>
+                &#9656 Bạn có <?php echo $row2["COUNT(*)"]; ?> thụ hưởng.<br>
+                &#9656 Giao dịch cuối cùng của bạn:<br>
+                &emsp;<?php echo $type; ?>: <?php echo number_format($transaction); ?> VND<br>
+                &emsp;Thời gian: <?php echo $sanitized_time; ?><br>
+                &emsp;<?php echo $row1["remarks"]; ?>.
             </p>
         </div>
     </div>
