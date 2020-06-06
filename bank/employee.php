9a86adb1617f275e3fe899779ed596da8cf1ce7c
daoquyet88 <49382785+daoquyet88@users.noreply.github.com>
2020-06-06 20:14:46 +0700

    Update employee.php
    cap nhap chưc năng

--- bank/employee.php
+++ bank/employee.php
@@ -53,6 +53,38 @@
         </div>
         <?php
         $uname = mysqli_real_escape_string($conn, $_POST["admin_uname"]);
+    $pwd = mysqli_real_escape_string($conn, $_POST["admin_psw"]);
+
+    $sql0 =  "SELECT * FROM admin WHERE uname='".$uname."' AND pwd='".$pwd."'";
+    $result = $conn->query($sql0);
+
+    if (($result->num_rows) > 0) {
+        $_SESSION['isAdminValid'] = true;
+        $_SESSION['LAST_ACTIVITY'] = time();
+        header("location:admin_home.php");
+    } else {
+        session_destroy();
+        die(header("location:admin_login.php?loginFailed=true"));
+    }?>
+    </form>
+    <div class="flex-container">
+        <div class="flex-item">
+            <h1 id="customer">
+                Xin chào, <?php echo $row0["first_name"] ?>&nbsp<?php echo $row0["last_name"] ?>!
+                <br>Số tài khoản: <?php echo $row0["account_no"]; ?>
+            </h1>
+            <p id="customer">
+                &#9656 Số dư (VND): <?php echo number_format($row1["balance"]); ?><br>
+                &#9656 Bạn có <?php echo $row2["COUNT(*)"]; ?> thụ hưởng.<br>
+                &#9656 Giao dịch cuối cùng của bạn:<br>
+                &emsp;<?php echo $type; ?>: <?php echo number_format($transaction); ?> VND<br>
+                &emsp;Thời gian: <?php echo $sanitized_time; ?><br>
+                &emsp;<?php echo $row1["remarks"]; ?>.
+            </p><?php
+            if (($result->num_rows) > 0) {
+        $_SESSION['isAdminValid'] = true;
+        $_SESSION['LAST_ACTIVITY'] = time();
     
+    </div>
 </body>
 </html>
