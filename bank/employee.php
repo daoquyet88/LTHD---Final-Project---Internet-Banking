59c39397bb0f13a6c029be286a6368d104f4ac9c
daoquyet88 <49382785+daoquyet88@users.noreply.github.com>
2020-06-06 20:14:17 +0700

    Create employee.php
    lam trang employee

---
+++ bank/employee.php
@@ -0,0 +1,58 @@
+
+<?php
+    include "header.php";
+
+    if (isset($_GET['loginFailed'])) {
+        $message = "Thông tin không hợp lệ! Vui lòng thử lại.";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/admin_login_style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+
+<body>
+    <form action="admin_login_action.php" method="post">
+        <div class="flex-container-1">
+            <div class="flex-item">
+                <h2>Đăng nhập quản trị viên</h2>
+            </div>
+
+            <label><b>Tài khoản</b></label>
+            <div class="flex-item">
+                <input type="text" name="admin_uname" required>
+            </div>
+
+            <label><b>Mật khẩu</b></label>
+            <div class="flex-item">
+                <input type="password" name="admin_psw" required>
+            </div>
+        </div>
+
+        <div class="flex-container-2">
+            <div class="flex-item">
+                <button type="submit">Đăng nhập</button>
+            </div>
+
+            <div class="flex-item">
+                <button type="button" class="cancelbtn" onclick="location.href='./home.php'">Quay về trang chủ</button>
+            </div>
+        </div>
+        <div class="flex-container-2">
+            <div class="flex-item">
+                <button type="submit">Đăng nhập</button>
+            </div>
+
+            <div class="flex-item">
+                <button type="button" class="cancelbtn" onclick="location.href='./home.php'">Quay về trang chủ</button>
+            </div>
+        </div>
+        <?php
+        $uname = mysqli_real_escape_string($conn, $_POST["admin_uname"]);
+    
+</body>
+</html>
